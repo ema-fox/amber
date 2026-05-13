12e33d9ba325e5b08660fcec9654263e98b40e38
Emanuel Rylke <ema@mailbox.org>
2026-05-13 14:47:15 +0200

    Ep. 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-struct AFn(Box<dyn Fn (Val) -> Result<Val, Val>>);
+use std::rc::Rc;
+
+#[derive(Clone)]
+struct AFn(Rc<dyn Fn (Val) -> Result<Val, Val>>);
 
 impl std::fmt::Debug for AFn {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -6,7 +9,7 @@ impl std::fmt::Debug for AFn {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Val {
     Int(i64),
     List(Vec<Val>),
@@ -15,14 +18,15 @@ enum Val {
 
 #[derive(Debug)]
 enum Inst {
-    Int(i64),
+    Lit(Val),
     List(Vec<Inst>),
-    Call(Box<Inst>, Box<Inst>)
+    Call(Box<Inst>, Box<Inst>),
+    If(Box<Inst>, Box<Inst>, Box<Inst>)
 }
 
 fn eval(inst: &Inst) -> Result<Val, Val> {
     match inst {
-        Inst::Int(x) => Ok(Val::Int(*x)),
+        Inst::Lit(x) => Ok(x.clone()),
         Inst::List(xs) => Ok(Val::List(xs.iter().map(|x| eval(x).unwrap()).collect())),
         Inst::Call(finst, arginst) => {
             let f = eval(finst).unwrap();
@@ -32,6 +36,12 @@ fn eval(inst: &Inst) -> Result<Val, Val> {
                     f2.0(arg)
                 },
                 _ => panic!()
+            }
+        },
+        Inst::If(cond_inst, then_inst, else_inst) => {
+            match eval(cond_inst) {
+                Ok(_) => eval(then_inst),
+                Err(_) => eval(else_inst)
             }
         }
     }
@@ -45,14 +55,35 @@ fn plus(arg: Val) -> Result<Val, Val> {
                     Val::Int(n) => n,
                     _ => panic!()
                 }
-            })).sum()
+            }).sum()))
         },
         _ => panic!()
     }
 }
 
+fn lt(arg: Val) -> Result<Val, Val> {
+    match arg {
+        Val::List(xs) => {
+            match (xs[0].clone(), xs[1].clone()) {
+                (Val::Int(first), Val::Int(second)) => {
+                    if first < second {
+                        Ok(xs[0].clone())
+                    } else {
+                        Err(xs[0].clone())
+                    }
+                },
+                _ => panic!()
+            }
+        },
+        _ => panic!()
+    }
+}
 
 fn main() {
-    let code = Inst::List(vec![Inst::Int(2), Inst::Int(3)]);
+    // {if (< 2 3) 0 99}
+    let code = Inst::If(Box::new(Inst::Call(Box::new(Inst::Lit(Val::Fn(AFn(Rc::new(lt))))),
+                                            Box::new(Inst::List(vec![Inst::Lit(Val::Int(2)), Inst::Lit(Val::Int(3))])))),
+                        Box::new(Inst::Lit(Val::Int(0))),
+                        Box::new(Inst::Lit(Val::Int(99))));
     dbg!(eval(&code));
 }
