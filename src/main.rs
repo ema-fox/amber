1c8a26e44fda7b985ee20cd219d79f744ed2dcd8
Emanuel Rylke <ema@mailbox.org>
2026-05-18 21:54:57 +0200

    Ep. 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::rc::Rc;
+use std::collections::HashMap;
 
 use nom::{IResult, Parser};
 use nom::branch::{alt};
-use nom::character::complete::{char, alpha1, alphanumeric1, digit1, multispace0};
+use nom::character::complete::{char, one_of, alpha1, alphanumeric1, digit1, multispace0};
 use nom::combinator::{recognize, map};
 use nom::multi::{many0_count, many0};
 use nom::sequence::{delimited, preceded};
@@ -26,13 +27,18 @@ enum Val {
 #[derive(Debug, Clone)]
 enum Inst {
     Lit(Val),
+    Deref(String),
     List(Vec<Inst>),
     Call(Box<Inst>, Box<Inst>),
     If(Box<Inst>, Box<Inst>, Box<Inst>)
 }
 
+fn sym_char(inp: &str) -> IResult<&str, &str> {
+    recognize(one_of("<+")).parse(inp)
+}
+
 fn psym(inp: &str) -> IResult<&str, &str> {
-    recognize((alpha1, many0_count(alphanumeric1))).parse(inp)
+    recognize((alt((alpha1, sym_char)), many0_count(alt((alphanumeric1, sym_char))))).parse(inp)
 }
 
 fn pnum(inp: &str) -> IResult<&str, &str> {
@@ -43,9 +49,16 @@ fn plit(inp: &str) -> IResult<&str, Inst> {
     map(pnum, |v: &str| Inst::Lit(Val::Int(i64::from_str_radix(v, 10).unwrap()))).parse(inp)
 }
 
+fn pderef(inp: &str) -> IResult<&str, Inst> {
+    map(psym, |v: &str| Inst::Deref(v.to_string())).parse(inp)
+}
+
 fn pbraceinst(inp: &str) -> IResult<&str, Inst> {
     map(delimited(char('{'), (psym, pinsts), char('}')),
         |(op, args): (&str, Vec<Inst>)| match op {
+            "list" => Inst::List(args),
+            "call" => Inst::Call(Box::new(args[0].clone()),
+                                 Box::new(args[1].clone())),
             "if" => Inst::If(Box::new(args[0].clone()),
                              Box::new(args[1].clone()),
                              Box::new(args[2].clone())),
@@ -53,21 +66,34 @@ fn pbraceinst(inp: &str) -> IResult<&str, Inst> {
         }).parse(inp)
 }
 
+fn plistinst(inp: &str) -> IResult<&str, Inst> {
+    map(delimited(char('['), pinsts, char(']')),
+        |entries: Vec<Inst>| Inst::List(entries)
+    ).parse(inp)
+}
+
+fn pcallinst(inp: &str) -> IResult<&str, Inst> {
+    map(delimited(char('('), (pinst, pinsts), char(')')),
+        |(f, args): (Inst, Vec<Inst>)| Inst::Call(Box::new(f), Box::new(Inst::List(args)))
+    ).parse(inp)
+}
+
 fn pinst(inp: &str) -> IResult<&str, Inst> {
-    alt((plit, pbraceinst)).parse(inp)
+    alt((plit, pderef, pbraceinst, plistinst, pcallinst)).parse(inp)
 }
 
 fn pinsts(inp: &str) -> IResult<&str, Vec<Inst>> {
     many0(preceded(multispace0, pinst)).parse(inp)
 }
 
-fn eval(inst: &Inst) -> Result<Val, Val> {
+fn eval(inst: &Inst, env: &HashMap<String, Val>) -> Result<Val, Val> {
     match inst {
         Inst::Lit(x) => Ok(x.clone()),
-        Inst::List(xs) => Ok(Val::List(xs.iter().map(|x| eval(x).unwrap()).collect())),
+        Inst::Deref(x) => Ok(env.get(x).unwrap().clone()),
+        Inst::List(xs) => Ok(Val::List(xs.iter().map(|x| eval(x, env).unwrap()).collect())),
         Inst::Call(finst, arginst) => {
-            let f = eval(finst).unwrap();
-            let arg = eval(arginst).unwrap();
+            let f = eval(finst, env).unwrap();
+            let arg = eval(arginst, env).unwrap();
             match f {
                 Val::Fn(f2) => {
                     f2.0(arg)
@@ -76,9 +102,9 @@ fn eval(inst: &Inst) -> Result<Val, Val> {
             }
         },
         Inst::If(cond_inst, then_inst, else_inst) => {
-            match eval(cond_inst) {
-                Ok(_) => eval(then_inst),
-                Err(_) => eval(else_inst)
+            match eval(cond_inst, env) {
+                Ok(_) => eval(then_inst, env),
+                Err(_) => eval(else_inst, env)
             }
         }
     }
@@ -118,6 +144,8 @@ fn lt(arg: Val) -> Result<Val, Val> {
 
 fn main() {
     // {if (< 2 3) 0 99}
+    // {if {call < {list 2 3}} 0 99}
+    /*
     let code = Inst::If(Box::new(Inst::Call(Box::new(Inst::Lit(Val::Fn(AFn(Rc::new(lt))))),
                                             Box::new(Inst::List(vec![Inst::Lit(Val::Int(2)), Inst::Lit(Val::Int(3))])))),
                         Box::new(Inst::Lit(Val::Int(0))),
@@ -125,5 +153,12 @@ fn main() {
     dbg!(eval(&code));
 
     dbg!(pinst("{if 2 3 4}"));
+    */
+
+    let glob: HashMap<String, Val> = [
+        ("<".to_string(), Val::Fn(AFn(Rc::new(lt)))),
+        ("+".to_string(), Val::Fn(AFn(Rc::new(plus))))
+    ].into();
+    dbg!(eval(&pinst("{if (< 4 3) 0 (+ 90 9)}").unwrap().1, &glob));
     //dbg!(pnum("123"));
 }
