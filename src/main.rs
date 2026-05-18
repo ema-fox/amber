ff5d184a559dd02c82d20acf08623962368b652c
Emanuel Rylke <ema@mailbox.org>
2026-05-18 18:08:16 +0200

    Ep. 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::rc::Rc;
 
+use nom::{IResult, Parser};
+use nom::branch::{alt};
+use nom::character::complete::{char, alpha1, alphanumeric1, digit1, multispace0};
+use nom::combinator::{recognize, map};
+use nom::multi::{many0_count, many0};
+use nom::sequence::{delimited, preceded};
+
 #[derive(Clone)]
 struct AFn(Rc<dyn Fn (Val) -> Result<Val, Val>>);
 
@@ -16,12 +23,42 @@ enum Val {
     Fn(AFn)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Inst {
     Lit(Val),
     List(Vec<Inst>),
     Call(Box<Inst>, Box<Inst>),
     If(Box<Inst>, Box<Inst>, Box<Inst>)
+}
+
+fn psym(inp: &str) -> IResult<&str, &str> {
+    recognize((alpha1, many0_count(alphanumeric1))).parse(inp)
+}
+
+fn pnum(inp: &str) -> IResult<&str, &str> {
+    digit1.parse(inp)
+}
+
+fn plit(inp: &str) -> IResult<&str, Inst> {
+    map(pnum, |v: &str| Inst::Lit(Val::Int(i64::from_str_radix(v, 10).unwrap()))).parse(inp)
+}
+
+fn pbraceinst(inp: &str) -> IResult<&str, Inst> {
+    map(delimited(char('{'), (psym, pinsts), char('}')),
+        |(op, args): (&str, Vec<Inst>)| match op {
+            "if" => Inst::If(Box::new(args[0].clone()),
+                             Box::new(args[1].clone()),
+                             Box::new(args[2].clone())),
+            _ => todo!()
+        }).parse(inp)
+}
+
+fn pinst(inp: &str) -> IResult<&str, Inst> {
+    alt((plit, pbraceinst)).parse(inp)
+}
+
+fn pinsts(inp: &str) -> IResult<&str, Vec<Inst>> {
+    many0(preceded(multispace0, pinst)).parse(inp)
 }
 
 fn eval(inst: &Inst) -> Result<Val, Val> {
@@ -86,4 +123,7 @@ fn main() {
                         Box::new(Inst::Lit(Val::Int(0))),
                         Box::new(Inst::Lit(Val::Int(99))));
     dbg!(eval(&code));
+
+    dbg!(pinst("{if 2 3 4}"));
+    //dbg!(pnum("123"));
 }
